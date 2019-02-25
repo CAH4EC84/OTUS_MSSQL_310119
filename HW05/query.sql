dce5a614e8e7d80f6cb1ce56707cdd776f94fb73
Александр <cah4ec84@yandex.ru>
2019-02-25 12:19:59 +0300

    Исправления дз

--- HW05/query.sql
+++ HW05/query.sql
@@ -1,50 +1,39 @@
 --1. Посчитать среднюю цену товара, общую сумму продажи по месяцам
 Select 
- [Description],
- AVG(UnitPrice) as AvgPrice,
+ W.StockItemName,
+ AVG(L.UnitPrice) as AvgPrice,
+ YEAR(O.OrderDate) as OrdYEar,
  MONTH(O.OrderDate) as OrdMonth,
- SUM(UnitPrice * Quantity)  as TotalMonthSumm
+ SUM(L.UnitPrice * Quantity)  as TotalMonthSumm
 FROM [Sales].[OrderLines] L 
 	inner JOIN Sales.Orders O on  L.OrderID = O.OrderID
-Group by  [Description],MONTH(O.OrderDate)
-order by  [Description],OrdMonth
-
+	inner join Warehouse.StockItems W on L.StockItemID = W.StockItemID
+Group by  W.StockItemName, YEAR(O.OrderDate),MONTH(O.OrderDate)
+order by  W.StockItemName,OrdYear,OrdMonth
 
 --2. Отобразить все месяцы, где общая сумма продаж превысила 10 000  (10 000 маловато)
 Select 
+ YEAR(O.OrderDate) as OrdYEar,
  MONTH(O.OrderDate) as OrdMonth
  --,Sum(UnitPrice * Quantity) as TotalSumm
 FROM [Sales].[OrderLines] L 
 	inner JOIN Sales.Orders O on  L.OrderID = O.OrderID
-GROUP BY MONTH(O.OrderDate)
+GROUP BY YEAR(O.OrderDate),MONTH(O.OrderDate)
 HAVING Sum(UnitPrice * Quantity)> 10000
-order by OrdMonth
+order by OrdYear,OrdMonth
 
 --3. Вывести сумму продаж, дату первой продажи и количество проданного по месяцам, по товарам, продажи которых менее 50 ед в месяц. (50 маловато)
 Select 
-	L.[Description],
+	W.StockItemName,
 	SUM(L.Quantity * L.UnitPrice) as TotalSumm,
 	MIN(O.OrderDate) as firstSaleDate,
 	SUM(Quantity) as saledQuantity,
+	YEAR(O.OrderDate) as Years,
 	MONTH(O.OrderDate) as Months
 FROM [Sales].[OrderLines] L 
 	inner JOIN Sales.Orders O on  L.OrderID = O.OrderID
-GROUP BY [Description],MONTH(O.OrderDate)
+	inner join Warehouse.StockItems W on L.StockItemID = W.StockItemID
+GROUP BY W.StockItemName,YEAR(O.OrderDate),MONTH(O.OrderDate)
 HAVING SUM(Quantity) < 50
-order by [Description],Months
-
---В задании указано по месяцам, в реальности по месяцам и годам т.к таблица содержит данные за 4 года и одинаковые месяца сгруппируются.
-Select 
-	L.[Description],
-	SUM(L.Quantity * L.UnitPrice) as TotalSumm,
-	MIN(O.OrderDate) as firstSaleDate,
-	SUM(Quantity) as saledQuantity,
-	MONTH(O.OrderDate) as Months,
-	YEAR(O.OrderDate) as Years
-FROM [Sales].[OrderLines] L 
-	inner JOIN Sales.Orders O on  L.OrderID = O.OrderID
-GROUP BY [Description],MONTH(O.OrderDate),YEAR(O.OrderDate) 
-HAVING SUM(Quantity) < 100
-order by [Description],Months,Years
-
+order by W.StockItemName,Years,Months
 
