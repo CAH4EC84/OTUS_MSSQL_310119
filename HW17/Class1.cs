4aff35c4a1c441c5215eafe6a89556e9f054d209
Александр <cah4ec84@yandex.ru>
2019-06-26 13:42:52 +0300

    Update Class1.cs

--- HW17/Class1.cs
+++ HW17/Class1.cs
@@ -6,25 +6,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-public class MailSender{
-    public static int Send(string subject, string mailTo, string attach)
+public class MailSender
+{
+    public static string Send(string subject, string mailTo)
     {
-        try { 
+        try
+        {
+            /*
+            string subject = "Report Complite";
+            string mailTo = "alex@russia.spb.ru";
+            */
             string mailFrom = "robot@medline.spb.ru";
-            string attachPath = @"D:\MedlineAdminSoft\Temp\BIRJA.INI";
-            System.Net.Mail.SmtpClient client = new SmtpClient("mx.russia.spb.ru", 25);
+            System.Net.Mail.SmtpClient client = new SmtpClient("mx.russia.spb.ru");
             client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("robot@russia.spb.ru", "somepassword");
+            client.EnableSsl = false;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.Credentials = new System.Net.NetworkCredential("robot@russia.spb.ru", "somepasswd");
+
             MailMessage mailMessage = new MailMessage(mailFrom, mailTo);
             mailMessage.Subject = subject;
             mailMessage.Body = "Отчет сгенерирован";
             client.Send(mailMessage);
-            return 1;
+            return "OK";
         }
-        catch (Exception ex) {
-            return 0;
+        catch (Exception ex)
+        {
+            return ex.Message;
         }
     }
 
 }
+
